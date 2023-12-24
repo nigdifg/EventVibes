5b8c8768a25c9bb7bb0c8dd17ed98bbf3c9b5bdb
Anurag <2006535@kiit.ac.in>
2023-12-24 12:42:56 +0530

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import logo from '../assets/logo.png';
 
 function Logo({width='100px'}) {
   return (
-    <div>Blog Vibes</div>
+    <img className='w-11' src={logo} alt="logo" />
   )
 }
 
