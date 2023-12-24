7f28f22c9525d224a18b4ed8520e2d99ffe72e44
Anurag <2006535@kiit.ac.in>
2023-12-24 10:45:48 +0530

    website is almost ready!

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Signup as SignupComponent } from '../components'
+
+function Signup() {
+  return (
+    <div className='py-8'>
+      
+      <h1 className='text-3xl font-bold'>
+        <SignupComponent/>
+      </h1>
+
+    </div>
+  )
+}
+
+export default Signup
